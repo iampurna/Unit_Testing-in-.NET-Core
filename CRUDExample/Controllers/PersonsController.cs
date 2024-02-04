75dc3938782a75d32bffe7f583025569b6852e8d
iampurna <linkwithpurna@gmail.com>
2024-02-04 16:04:53 +0545

    first commit

---
+++ CRUDExample/Controllers/PersonsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUDExample.Controllers
+{
+    public class PersonsController : Controller
+    {
+        //private fields
+        private readonly IPersonsService _personsService;
+
+        //constructor
+        public PersonsController(IPersonsService personsService)
+        {
+            _personsService = personsService;
+        }
+
+        // GET: /<controller>/
+        [Route("persons/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<PersonResponse> persons = _personsService.GetAllPersons();
+            return View(); //Views/Persons/Index.cshtml
+        }
+    }
+}
+
